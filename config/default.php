9b15e087aa12d7c12f3aad50aa365d49a8a51caf
Fabian Flugund <fabian.flugund@gmail.com>
2014-03-11 11:18:55 +0100

    init

---
+++ config/default.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 
+ * Overrides for 'default' config mode.
+ * 
+ * @var Aura\Framework\System $system Aura system directories; when cast to
+ * a string, the Aura system root directory.
+ * 
+ * @var Aura\Autoload\Loader $loader The autoloader for the system.
+ * 
+ * @var Aura\Di\Container $di The DI container for the system.
+ * 
+ */
